2c84590693986dd7fc12eeaa1f5e8d37c575d9aa
phamus <sphamus@gmail.com>
2020-02-13 03:06:26 +0100

    Logout button ui test

--- test/header.test.js
+++ test/header.test.js
@@ -12,11 +12,11 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  // await browser.close();
 });
 
 test("THe header has the correct text", async () => {
-  const text = await page.$eval("a.left.brand-logo", el => el.innerHTML);
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
   expect(text).toEqual("Blogster");
 });
@@ -25,6 +25,37 @@ test("clicking login starts oauth flow", async () => {
   await page.click(".right a");
 
   const url = await page.url();
-
+  console.log(url);
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only("when signed in, shows logout button", async () => {
+  const id = "5e3400cf02e8c954a88c3b39";
+
+  const Buffer = require("safe-buffer").Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    "base64"
+  );
+
+  const Keygrip = require("keygrip");
+  const keys = require("../config/keys");
+
+  const keygrip = new Keygrip([keys.cookieKey]);
+
+  const sig = keygrip.sign("session=" + sessionString);
+
+  await page.setCookie({ name: "session", value: sessionString });
+  await page.setCookie({ name: "session.sig", value: sig });
+
+  await page.goto("localhost:3000");
+
+  const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+  expect(text).toEqual("Logout");
 });
